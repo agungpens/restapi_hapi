36eb6d34c46344f6213563c49e9f017d482aaf60
agungaldi190701 <agung20.mhs@akb.ac.id>
2023-05-21 12:48:33 +0700

    restructure with hapi plugin

---
+++ src/notesPlugin.js
@@ -0,0 +1,19 @@
+const notesPlugin = {
+    name: 'notes',
+    version: '1.0.0',
+    register: async (server, options) => {
+        // contoh, menetapkan routing untuk /notes
+        const notes = options.notes;
+        server.route([
+            {
+                method: 'GET',
+                path: '/notes',
+                handler: () => {
+                    return notes;
+                }
+            }
+        ])
+    },
+}
+
+module.exports = notesPlugin;

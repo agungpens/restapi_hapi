b882d0a63106f655aa8226ab2f3e72de01703215
agungaldi190701 <agung20.mhs@akb.ac.id>
2023-05-21 15:40:32 +0700

    penambahan plugin validation "joi"

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -1,6 +1,7 @@
 class NotesHandler {
-    constructor(service) {
+    constructor(service, validator) {
         this._service = service;
+        this._validator = validator;
 
         this.postNoteHandler = this.postNoteHandler.bind(this);
         this.getNotesHandler = this.getNotesHandler.bind(this);
@@ -12,8 +13,9 @@ class NotesHandler {
     postNoteHandler(request, h) {
 
         try {
+            this._validator.validateNotePayload(request.payload);
             const { title = 'untitled', body, tags } = request.payload;
-            this._service.addNote({ title, body, tags });
+            const noteId = this._service.addNote({ title, body, tags });
             const response = h.response({
                 status: 'success',
                 message: 'Catatan berhasil ditambahkan',
@@ -68,6 +70,7 @@ class NotesHandler {
     // putNoteByIdHandler
     putNoteByIdHandler(request, h) {
         try {
+            this._validator.validateNotePayload(request.payload);
             const { id } = request.params;
             this._service.editNoteById(id, request.payload);
             return {
